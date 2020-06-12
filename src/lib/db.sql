b68a98bef56f8093d26512e33f33772010bb0e79
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-06-11 23:40:27 -0600

    File structure finished

---
+++ src/lib/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE People;
+
+USE People;
+
+CREATE TABLE Persons(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    tel VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
